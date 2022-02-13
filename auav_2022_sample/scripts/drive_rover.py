e496c5aef1ad5ffa393414e78309d98838eef0d4
James Goppert <james.goppert@gmail.com>
2022-02-13 18:40:00 -0500

    Add axis damping for rover.

--- auav_2022_sample/scripts/drive_rover.py
+++ auav_2022_sample/scripts/drive_rover.py
@@ -11,10 +11,30 @@ if __name__ == '__main__':
   apply_joint_effort = rospy.ServiceProxy('/gazebo/apply_joint_effort', ApplyJointEffort)
   try:
     rospy.loginfo('applying joint effort')
-    resp1 = apply_joint_effort('axial_scx10::right_rear_wheel_hinge', 1,
+    print('going right')
+    resp1 = apply_joint_effort('axial_scx10::right_rear_wheel_hinge', -0.1,
             rospy.Duration.from_sec(0),
             rospy.Duration.from_sec(-1))
-    resp1 = apply_joint_effort('axial_scx10::left_rear_wheel_hinge', 1,
+    print(resp1)
+    resp1 = apply_joint_effort('axial_scx10::left_rear_wheel_hinge', -0.1,
+            rospy.Duration.from_sec(0),
+            rospy.Duration.from_sec(-1))
+    print(resp1)
+    resp1 = apply_joint_effort('axial_scx10::left_front_steer_hinge', -0.1,
+            rospy.Duration.from_sec(0),
+            rospy.Duration.from_sec(-1))
+    print(resp1)
+    resp1 = apply_joint_effort('axial_scx10::right_front_steer_hinge', -0.1,
+            rospy.Duration.from_sec(0),
+            rospy.Duration.from_sec(-1))
+    print(resp1)
+    time.sleep(3)
+    print('going left')
+    resp1 = apply_joint_effort('axial_scx10::left_front_steer_hinge', 0.1,
+            rospy.Duration.from_sec(0),
+            rospy.Duration.from_sec(-1))
+    print(resp1)
+    resp1 = apply_joint_effort('axial_scx10::right_front_steer_hinge', 0.1,
             rospy.Duration.from_sec(0),
             rospy.Duration.from_sec(-1))
   except rospy.ServiceException as e:
