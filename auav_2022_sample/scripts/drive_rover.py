d3b4e853e0b9177c5780db2993addf0908204fdd
James Goppert <james.goppert@gmail.com>
2022-02-13 18:50:03 -0500

    Autolaunch rover drive script in sim.

--- auav_2022_sample/scripts/drive_rover.py
+++ auav_2022_sample/scripts/drive_rover.py
@@ -12,29 +12,29 @@ if __name__ == '__main__':
   try:
     rospy.loginfo('applying joint effort')
     print('going right')
-    resp1 = apply_joint_effort('axial_scx10::right_rear_wheel_hinge', -0.1,
+    resp1 = apply_joint_effort('rover::right_rear_wheel_hinge', -0.1,
             rospy.Duration.from_sec(0),
             rospy.Duration.from_sec(-1))
     print(resp1)
-    resp1 = apply_joint_effort('axial_scx10::left_rear_wheel_hinge', -0.1,
+    resp1 = apply_joint_effort('rover::left_rear_wheel_hinge', -0.1,
             rospy.Duration.from_sec(0),
             rospy.Duration.from_sec(-1))
     print(resp1)
-    resp1 = apply_joint_effort('axial_scx10::left_front_steer_hinge', -0.1,
+    resp1 = apply_joint_effort('rover::left_front_steer_hinge', -0.1,
             rospy.Duration.from_sec(0),
             rospy.Duration.from_sec(-1))
     print(resp1)
-    resp1 = apply_joint_effort('axial_scx10::right_front_steer_hinge', -0.1,
+    resp1 = apply_joint_effort('rover::right_front_steer_hinge', -0.1,
             rospy.Duration.from_sec(0),
             rospy.Duration.from_sec(-1))
     print(resp1)
     time.sleep(3)
     print('going left')
-    resp1 = apply_joint_effort('axial_scx10::left_front_steer_hinge', 0.1,
+    resp1 = apply_joint_effort('rover::left_front_steer_hinge', 0.1,
             rospy.Duration.from_sec(0),
             rospy.Duration.from_sec(-1))
     print(resp1)
-    resp1 = apply_joint_effort('axial_scx10::right_front_steer_hinge', 0.1,
+    resp1 = apply_joint_effort('rover::right_front_steer_hinge', 0.1,
             rospy.Duration.from_sec(0),
             rospy.Duration.from_sec(-1))
   except rospy.ServiceException as e:
