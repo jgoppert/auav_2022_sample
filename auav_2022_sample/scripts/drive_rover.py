5d45d25b656c0a0c011271f979676efa31ea04ac
James Goppert <james.goppert@gmail.com>
2022-02-14 02:17:31 -0500

    Work on rover model.

--- auav_2022_sample/scripts/drive_rover.py
+++ auav_2022_sample/scripts/drive_rover.py
@@ -9,33 +9,46 @@ if __name__ == '__main__':
   rospy.loginfo('starting rover controller, waiting for apply joint effort service')
   rospy.wait_for_service('/gazebo/apply_joint_effort')
   apply_joint_effort = rospy.ServiceProxy('/gazebo/apply_joint_effort', ApplyJointEffort)
-  try:
-    rospy.loginfo('applying joint effort')
-    print('going right')
-    resp1 = apply_joint_effort('rover::right_rear_wheel_hinge', -0.1,
-            rospy.Duration.from_sec(0),
-            rospy.Duration.from_sec(-1))
-    print(resp1)
-    resp1 = apply_joint_effort('rover::left_rear_wheel_hinge', -0.1,
-            rospy.Duration.from_sec(0),
-            rospy.Duration.from_sec(-1))
-    print(resp1)
-    resp1 = apply_joint_effort('rover::left_front_steer_hinge', -0.1,
-            rospy.Duration.from_sec(0),
-            rospy.Duration.from_sec(-1))
-    print(resp1)
-    resp1 = apply_joint_effort('rover::right_front_steer_hinge', -0.1,
-            rospy.Duration.from_sec(0),
-            rospy.Duration.from_sec(-1))
-    print(resp1)
-    time.sleep(3)
-    print('going left')
-    resp1 = apply_joint_effort('rover::left_front_steer_hinge', 0.1,
-            rospy.Duration.from_sec(0),
-            rospy.Duration.from_sec(-1))
-    print(resp1)
-    resp1 = apply_joint_effort('rover::right_front_steer_hinge', 0.1,
-            rospy.Duration.from_sec(0),
-            rospy.Duration.from_sec(-1))
-  except rospy.ServiceException as e:
-    rospy.logerror("Service did not process request: %s"%str(e))
+
+  while not rospy.is_shutdown():
+      try:
+        rospy.loginfo('going straight')
+        resp = apply_joint_effort('rover::right_rear_wheel_hinge', -0.2,
+                rospy.Duration.from_sec(0),
+                rospy.Duration.from_sec(10))
+        resp = apply_joint_effort('rover::left_rear_wheel_hinge', -0.2,
+                rospy.Duration.from_sec(0),
+                rospy.Duration.from_sec(10))
+        time.sleep(10)
+
+        rospy.loginfo('turn right')
+        resp = apply_joint_effort('rover::right_rear_wheel_hinge', -0.2,
+                rospy.Duration.from_sec(0),
+                rospy.Duration.from_sec(3))
+        resp = apply_joint_effort('rover::left_rear_wheel_hinge', -0.2,
+                rospy.Duration.from_sec(0),
+                rospy.Duration.from_sec(3))
+        resp = apply_joint_effort('rover::left_front_steer_hinge', -0.1,
+                rospy.Duration.from_sec(0),
+                rospy.Duration.from_sec(3))
+        resp = apply_joint_effort('rover::right_front_steer_hinge', -0.1,
+                rospy.Duration.from_sec(0),
+                rospy.Duration.from_sec(3))
+        time.sleep(3)
+
+        rospy.loginfo('turn left')
+        resp = apply_joint_effort('rover::right_rear_wheel_hinge', -0.2,
+                rospy.Duration.from_sec(0),
+                rospy.Duration.from_sec(3))
+        resp = apply_joint_effort('rover::left_rear_wheel_hinge', -0.2,
+                rospy.Duration.from_sec(0),
+                rospy.Duration.from_sec(3))
+        resp = apply_joint_effort('rover::left_front_steer_hinge', 0.1,
+                rospy.Duration.from_sec(0),
+                rospy.Duration.from_sec(3))
+        resp = apply_joint_effort('rover::right_front_steer_hinge', 0.1,
+                rospy.Duration.from_sec(0),
+                rospy.Duration.from_sec(3))
+        time.sleep(3)
+      except rospy.ServiceException as e:
+        rospy.logerr("Service did not process request: %s"%str(e))
