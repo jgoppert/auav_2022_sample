f96ac94c70bd9e6512eb52986f3fd73396e775e6
James Goppert <james.goppert@gmail.com>
2022-03-27 22:36:24 +0000

    Add rover i2c driver.

---
+++ auav_2022_sample/scripts/motor.py
@@ -0,0 +1,78 @@
+import time
+from PCA9685 import PCA9685
+
+
+class Motor:
+    def __init__(self):
+        self.pwm = PCA9685(0x40, debug=True)
+        self.pwm.setPWMFreq(50)
+    def duty_range(self,duty1,duty2,duty3,duty4):
+        if duty1>4095:
+            duty1=4095
+        elif duty1<-4095:
+            duty1=-4095        
+        
+        if duty2>4095:
+            duty2=4095
+        elif duty2<-4095:
+            duty2=-4095
+            
+        if duty3>4095:
+            duty3=4095
+        elif duty3<-4095:
+            duty3=-4095
+            
+        if duty4>4095:
+            duty4=4095
+        elif duty4<-4095:
+            duty4=-4095
+        return duty1,duty2,duty3,duty4
+        
+    def left_Upper_Wheel(self,duty):
+        if duty>0:
+            self.pwm.setMotorPwm(0,0)
+            self.pwm.setMotorPwm(1,duty)
+        elif duty<0:
+            self.pwm.setMotorPwm(1,0)
+            self.pwm.setMotorPwm(0,abs(duty))
+        else:
+            self.pwm.setMotorPwm(0,4095)
+            self.pwm.setMotorPwm(1,4095)
+    def left_Lower_Wheel(self,duty):
+        if duty>0:
+            self.pwm.setMotorPwm(3,0)
+            self.pwm.setMotorPwm(2,duty)
+        elif duty<0:
+            self.pwm.setMotorPwm(2,0)
+            self.pwm.setMotorPwm(3,abs(duty))
+        else:
+            self.pwm.setMotorPwm(2,4095)
+            self.pwm.setMotorPwm(3,4095)
+    def right_Upper_Wheel(self,duty):
+        if duty>0:
+            self.pwm.setMotorPwm(6,0)
+            self.pwm.setMotorPwm(7,duty)
+        elif duty<0:
+            self.pwm.setMotorPwm(7,0)
+            self.pwm.setMotorPwm(6,abs(duty))
+        else:
+            self.pwm.setMotorPwm(6,4095)
+            self.pwm.setMotorPwm(7,4095)
+    def right_Lower_Wheel(self,duty):
+        if duty>0:
+            self.pwm.setMotorPwm(4,0)
+            self.pwm.setMotorPwm(5,duty)
+        elif duty<0:
+            self.pwm.setMotorPwm(5,0)
+            self.pwm.setMotorPwm(4,abs(duty))
+        else:
+            self.pwm.setMotorPwm(4,4095)
+            self.pwm.setMotorPwm(5,4095)
+            
+ 
+    def setMotorModel(self,duty1,duty2,duty3,duty4):
+        duty1,duty2,duty3,duty4=self.duty_range(duty1,duty2,duty3,duty4)
+        self.left_Upper_Wheel(duty1)
+        self.left_Lower_Wheel(duty2)
+        self.right_Upper_Wheel(duty3)
+        self.right_Lower_Wheel(duty4)
---
+++ auav_2022_sample/scripts/rover_i2c_driver.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import rospy
+from geometry_msgs.msg import Twist
+import time
+from PCA9685 import PCA9685
+from motor import Motor
+
+
+class RoverI2cDriver:
+
+    def __init__(self):
+        self.motor=Motor()          
+        rospy.init_node('rover_i2c_driver')
+        self.cmd_sub = rospy.Subscriber('cmd_vel', Twist, self.cmd_callback)
+
+    def move(self, vel, omega):
+        scale = 2000
+        speed_max = 2000
+        lspeed = int(scale*(vel + omega))
+        rspeed = int(scale*(vel - omega))
+        if abs(lspeed) > speed_max:
+            lspeed = speed_max*lspeed/abs(lspeed)
+        if abs(rspeed) > speed_max:
+            rspeed = speed_max*rspeed/abs(rspeed)
+        self.motor.setMotorModel(
+                lspeed, lspeed, rspeed, rspeed)
+
+    def __del__(self):
+        self.move(vel=0, omega=0)
+
+    def cmd_callback(self, msg):
+        rospy.loginfo("vel: %g oemga: %g", msg.linear.x, msg.angular.z)
+        self.move(vel=msg.linear.x, omega=msg.angular.z)
+        
+          
+if __name__ == "__main__":
+    driver = RoverI2cDriver()
+    rospy.spin()
