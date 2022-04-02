62ea85c0c0b26d609b75314c79485dee1e557e58
James Goppert <james.goppert@gmail.com>
2022-04-02 03:26:24 -0400

    Add roi to 3d point example, tie into color tracking.

--- auav_2022_sample/scripts/color_tracking.py
+++ auav_2022_sample/scripts/color_tracking.py
@@ -1,140 +1,80 @@
-#!/usr/bin/env python3.8
+#!/usr/bin/env python3
 import rospy
 from std_msgs.msg import Header
-from geometry_msgs.msg import Quaternion, QuaternionStamped, PoseStamped
-from sensor_msgs.msg import Image
+from geometry_msgs.msg import PoseStamped
+from sensor_msgs.msg import Image, CameraInfo
 from cv_bridge import CvBridge
 import cv2
 import numpy as np
-
-# The detection
-bridge = CvBridge()
-
-pub = rospy.Publisher("/drone/rover/bounding_box", QuaternionStamped, queue_size=1)
-img_pub = rospy.Publisher("/drone/rover/image", Image, queue_size=1)
-
-def box_from_path(img_path, debug=False):
-    return box_from_image(cv2.imread(img_path), debug)
-
-def box_from_image(img, debug=False, show=False):
-    #cv2.imshow("Input Image", img)
-    x_pos = -1
-    width = 0
-    y_pos = -1
-    height = 0
-
-    #img = resize_with_max(img, 800)
-
-    if debug:
-        print("Image shape: {0}".format(img.shape))
-        cv2.imshow("Input Image", img)
-
-    mask = None
-
-    # This is a color image. Look for the largest red object.
-    # L(lightness) A (+redness vs greenness) B (+yellowness vs blueness)
-    lower_range = np.array([0, 160, 0])
-    upper_range = np.array([255, 255, 255])
-    lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
-    mask = cv2.inRange(lab, lower_range, upper_range)
-    #cv2.imshow('Mask', mask)
-
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(10, 10))
-    mask = cv2.erode(mask, kernel, iterations=1)
-    mask = cv2.dilate(mask, kernel, iterations=1)
-    #cv2.imshow('Erode Dilate', mask)
-    #cv2.waitKey(1)
-
-    #contours = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
-    contours = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[0]
-
-    # Iterate through contours to find the one with the largest area
-    max_area = -1
-
-    if len(contours) > 10:
-        rospy.logwarn('contours: {:d}'.format(len(contours)))
-
-    contour = None
-    for cur in contours:
-        
-        if cur is None:
-            continue
-        cur_area = cv2.contourArea(cur)
-        if cur_area > max_area:
-            max_area = cur_area
-            contour = cur
-       
-    # minimum size of red object
-    # if max_area < 50:
-        # return {"x1": x_pos, "y1": y_pos, "x2": x_pos + width, "y2": y_pos + height, "width": width, "height": height}
-
-    boundingRect = cv2.boundingRect(contour)
-    if (boundingRect[2] > 5):
-        x_pos, y_pos, width, height = boundingRect
-    
-    print("x pos: {0}", x_pos)
-    
-    if debug:
-        print("Rectangle at position ({0}, {1}), size ({2}, {3})".format(x_pos, y_pos, width, height))
-
-    if show or debug:
-        if (x_pos != 0):
-            img = cv2.rectangle(img, (x_pos, y_pos), (x_pos + width, y_pos + height), (255, 0, 0), 5)
-        img_pub.publish(bridge.cv2_to_imgmsg(img, "bgr8"))
-
-    # Now, just normalize all the coordinates
-    x_pos = x_pos * 1.0 / img.shape[1]
-    y_pos = y_pos * 1.0 / img.shape[0]
-    width = width * 1.0 / img.shape[1]
-    height = height * 1.0 / img.shape[0]
-
-    box = {"x1": x_pos, "y1": y_pos, "x2": x_pos + width, "y2": y_pos + height, "width": width, "height": height}
-    if debug:
-        print("Box: {0}".format(box))
+from copy import copy
 
 
-    return box
+class ColorDetection:
+    """
+    Detects color ball in image and publishes ROI
+    """
 
+    def __init__(self):
+        rospy.init_node('color_tracking', anonymous=True)
+        self.pub_roi = rospy.Publisher('rover/roi', CameraInfo, queue_size=1)
+        self.pub_img= rospy.Publisher('rover/image', Image, queue_size=1)
+        self.sub_camera_info = rospy.Subscriber('camera/color/camera_info',
+                CameraInfo, self.camera_info_callback)
+        self.sub_camera = rospy.Subscriber('camera/color/image_raw',
+                Image, self.image_callback, queue_size=1)
+        self.bridge = CvBridge()
+        self.camera_info = None
+        rospy.spin()
 
-def resize_with_max(img, max_dim, debug=False):
-    if debug:
-        print("Prior to resize: {0}".format(img.shape))
-    larger = 0
-    if img.shape[0] > img.shape[1]:
-        larger = img.shape[0]
-    else:
-        larger = img.shape[1]
+    def camera_info_callback(self, msg: CameraInfo):
+        """Callback from camera projetion"""
+        self.camera_info = msg
 
-    if larger > max_dim:
-        scale_factor = max_dim / larger
-        img = cv2.resize(img, None, img.size, scale_factor, scale_factor, cv2.INTER_LINEAR)
-    if debug:
-        print("After resize: {0}".format(img.shape))
-    return img
+    def image_callback(self, msg):
+        if self.camera_info is None:
+            rospy.logerr('no camera info')
+            return
 
-def image_callback(data):
-    cv_image = bridge.imgmsg_to_cv2(data, desired_encoding='bgr8')
-    result = box_from_image(cv_image, show=True, debug=False)
-    
-    msg = Quaternion()
-    msg.x = result["x1"]
-    msg.y = result["y1"]
-    msg.z = result["x2"]
-    msg.w = result["y2"]
-    print(result)
-    stamped_msg = QuaternionStamped()
-    stamped_msg.header = Header()
-    stamped_msg.header.stamp = rospy.Time.now()
-    stamped_msg.quaternion = msg
-    # rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-    pub.publish(stamped_msg)
+        img = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
 
+        # This is a color image. Look for the largest red object.
+        # L(lightness) A (+redness vs greenness) B (+yellowness vs blueness)
+        lower_range = np.array([0, 160, 0])
+        upper_range = np.array([255, 255, 255])
+        lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+        mask = cv2.inRange(lab, lower_range, upper_range)
 
-def start_detection_node():
- 
-    rospy.init_node('color_tracking', anonymous=True)
-    rospy.Subscriber("camera/color/image_raw", Image, image_callback, queue_size=1)
-    rospy.spin()
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(10, 10))
+        mask = cv2.erode(mask, kernel, iterations=1)
+        mask = cv2.dilate(mask, kernel, iterations=1)
+        contours = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[0]
+
+        # Iterate through contours to find the one with the largest area
+        max_area = -1
+        if len(contours) > 10:
+            rospy.logwarn('contours: {:d}'.format(len(contours)))
+
+        contour = None
+        for cur in contours:
+            if cur is None:
+                continue
+            cur_area = cv2.contourArea(cur)
+            if cur_area > max_area:
+                max_area = cur_area
+                contour = cur
+           
+        x, y, width, height = cv2.boundingRect(contour)
+        roi = copy(self.camera_info)
+        roi.header.stamp = msg.header.stamp
+        roi.roi.x_offset = x
+        roi.roi.y_offset = y
+        roi.roi.width = width
+        roi.roi.height = height
+        self.pub_roi.publish(roi)
+        self.pub_img.publish(self.bridge.cv2_to_imgmsg(img))
+        img_roi = cv2.rectangle(img, (x, y), (x + width, y + height), (255, 100, 100), 3)
+        self.pub_img.publish(self.bridge.cv2_to_imgmsg(img_roi, "bgr8"))
+
 
 if __name__ == '__main__':
-    start_detection_node()
+    ColorDetection()
---
+++ auav_2022_sample/scripts/roi_to_point.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+import rospy
+import numpy as np
+import tf2_ros
+from sensor_msgs.msg import CameraInfo
+from geometry_msgs.msg import PointStamped
+import tf.transformations
+
+
+class RoiToPoint:
+
+    """
+    Tracks rover in image
+    """
+
+    def __init__(self):
+        rospy.init_node('rover_tracking')
+        self.sub_roi = rospy.Subscriber('rover/roi', CameraInfo, self.roi_callback)
+        self.pub_point = rospy.Publisher("rover/point", PointStamped, queue_size=1)
+
+        self.camera_proj = None
+        self.object_width = rospy.get_param('object_width', 0.06)
+        self.tfBuffer = tf2_ros.Buffer()
+        self.tfListener = tf2_ros.TransformListener(self.tfBuffer)
+        rospy.spin()
+
+    def roi_callback(self, msg: CameraInfo):
+        """Callback from ROI"""
+        K = np.reshape(msg.K, (3, 3))
+        f_x = K[0, 0]
+        f_y = K[1, 1]
+        c_x = K[0, 2]
+        c_y = K[1, 2]
+
+        # see https://mayavan95.medium.com/3d-position-estimation-of-a-known-object-using-a-single-camera-7a82b37b326b
+        if msg.roi.width <= 0:
+            return
+        z = (f_x*self.object_width)/msg.roi.width
+        x = ((msg.roi.x_offset + msg.roi.width/2 - c_x)*z)/f_x
+        y = ((msg.roi.y_offset + msg.roi.height/2 - c_y)*z)/f_y
+        p_rel = np.array([x, y, z])
+
+        try:
+            trans = self.tfBuffer.lookup_transform('map', 'camera_link', msg.header.stamp, rospy.Duration(0.1)).transform
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
+            rospy.logerr_throttle(1, 'failed to find camera -> base_link:')
+            return
+        p_cam = np.array([trans.translation.x, trans.translation.y, trans.translation.z])
+        # rospy.loginfo('p_rel: %f %f %f', p_rel[0], p_rel[1], p_rel[2])
+        # rospy.loginfo('p_cam: %f %f %f', p_cam[0], p_cam[1], p_cam[2])
+        R_cam = tf.transformations.quaternion_matrix(
+                [trans.rotation.x, trans.rotation.y, trans.rotation.z, trans.rotation.w])[:3, :3]
+        # rospy.loginfo('R: [[%f %f %f],  [%f %f %f],  [%f %f %f]]',
+                # R_cam[0, 0], R_cam[0, 1], R_cam[0, 2],
+                # R_cam[1, 0], R_cam[1, 1], R_cam[1, 2],
+                # R_cam[2, 0], R_cam[2, 1], R_cam[2, 2])
+        v = R_cam@p_rel + p_cam
+        # rospy.loginfo('p: %f %f %f', v[0], v[1], v[2])
+        point = PointStamped()
+        point.header.frame_id = 'map'
+        point.header.stamp = msg.header.stamp
+        point.point.x = v[0]
+        point.point.y = v[1]
+        point.point.z = v[2]
+        self.pub_point.publish(point)
+
+
+if __name__ == '__main__':
+    RoiToPoint()
