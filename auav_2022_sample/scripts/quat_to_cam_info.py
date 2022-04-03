159a7977f1c47820e2828e68c6027a88e9d37577
Hunter Haglid <55119245+Hunter314@users.noreply.github.com>
2022-04-03 17:58:53 -0400

    Create quat_to_cam_info.py
    Created a file to temporarily convert object tracking's bounding box to a camera info message.

---
+++ auav_2022_sample/scripts/quat_to_cam_info.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+import rospy
+from std_msgs.msg import Header
+from geometry_msgs.msg import PoseStamped
+from sensor_msgs.msg import Image, CameraInfo, QuaternionStamped
+from cv_bridge import CvBridge
+import cv2
+import numpy as np
+from copy import copy
+
+
+class QuatToCamInfo:
+    """
+    A quick solution to take the bounding box stored in a quaternion and convert it to a Camera Info ROI
+    """
+
+    def __init__(self):
+        rospy.init_node('quat_to_cam_info', anonymous=True)
+        self.pub_roi = rospy.Publisher('rover/roi', CameraInfo, queue_size=1)
+        self.pub_img= rospy.Publisher('rover/image', Image, queue_size=1)
+        self.sub_camera_info = rospy.Subscriber('camera/color/camera_info',
+                CameraInfo, self.camera_info_callback)
+        self.sub_camera = rospy.Subscriber('rover/bounding_box',
+                QuaternionStamped, self.image_callback, queue_size=1)
+        self.bridge = CvBridge()
+        self.camera_info = None
+        rospy.spin()
+
+    def camera_info_callback(self, msg: CameraInfo):
+        """Callback from camera projetion"""
+        self.camera_info = msg
+
+    def image_callback(self, msg: QuaternionStamped):
+        if self.camera_info is None:
+            rospy.logerr('no camera info')
+            return
+
+        x = msg.quaternion.x
+        y = msg.quaternion.y
+        width = msg.quaternion.z
+        height = msg.quaternion.w
+        
+        roi = copy(self.camera_info)
+        roi.header.stamp = msg.header.stamp
+        roi.roi.x_offset = x
+        roi.roi.y_offset = y
+        roi.roi.width = width
+        roi.roi.height = height
+        self.pub_roi.publish(roi)
+        self.pub_img.publish(self.bridge.cv2_to_imgmsg(img))
+        img_roi = cv2.rectangle(img, (x, y), (x + width, y + height), (255, 100, 100), 3)
+        self.pub_img.publish(self.bridge.cv2_to_imgmsg(img_roi, "bgr8"))
+
+
+if __name__ == '__main__':
+    QuatToCamInfo()
