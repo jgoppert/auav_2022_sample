936d87b4e3de7a286bb1d0dd8a6a5e6817895be9
Hunter Haglid <hunter.haglid@gmail.com>
2022-04-03 18:26:24 -0400

    Added quat to roi conversion

--- auav_2022_sample/scripts/quat_to_cam_info.py
+++ auav_2022_sample/scripts/quat_to_cam_info.py
@@ -20,9 +20,9 @@ class QuatToCamInfo:
         self.pub_img= rospy.Publisher('rover/image', Image, queue_size=1)
         self.sub_camera_info = rospy.Subscriber('camera/color/camera_info',
                 CameraInfo, self.camera_info_callback)
-        self.sub_camera = rospy.Subscriber('rover/bounding_box',
+        self.sub_quaternion = rospy.Subscriber('rover/bounding_box',
                 QuaternionStamped, self.image_callback, queue_size=1)
-        self.bridge = CvBridge()
+        
         self.camera_info = None
         rospy.spin()
 
@@ -39,17 +39,17 @@ class QuatToCamInfo:
         y = msg.quaternion.y
         width = msg.quaternion.z
         height = msg.quaternion.w
-        
+        if x < 0 or y < 0:
+            x = 0
+            y = 0
+
         roi = copy(self.camera_info)
         roi.header.stamp = msg.header.stamp
-        roi.roi.x_offset = x
-        roi.roi.y_offset = y
-        roi.roi.width = width
-        roi.roi.height = height
-        self.pub_roi.publish(roi)
-        self.pub_img.publish(self.bridge.cv2_to_imgmsg(img))
-        img_roi = cv2.rectangle(img, (x, y), (x + width, y + height), (255, 100, 100), 3)
-        self.pub_img.publish(self.bridge.cv2_to_imgmsg(img_roi, "bgr8"))
+        roi.roi.x_offset = int(x)
+        roi.roi.y_offset = int(y)
+        roi.roi.width = int(width + 0.5)
+        roi.roi.height = int(height + 0.5)
+        self.pub_roi.publish(roi)   
 
 
 if __name__ == '__main__':
