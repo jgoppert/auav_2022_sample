2aa54fb50189375c82e61e2ddaf6cb07808e26de
Alex Rogers <acrogers307@gmail.com>
2022-04-09 17:05:54 -0400

    Update roi to point to work with real camera.

--- auav_2022_sample/scripts/offboard.py
+++ auav_2022_sample/scripts/offboard.py
@@ -2,10 +2,13 @@
 import rospy
 import math
 import numpy as np
+import ros_numpy
+import sensor_msgs.point_cloud2 as pc2
 from geometry_msgs.msg import PoseStamped, Quaternion, PointStamped
 from mavros_msgs.msg import ExtendedState, State, ParamValue
 from mavros_msgs.srv import ParamGet, ParamSet
 from sensor_msgs.msg import Imu
+from sensor_msgs.msg import PointCloud2
 from pymavlink import mavutil
 from std_msgs.msg import Header, Bool
 from threading import Thread
@@ -18,6 +21,7 @@ class MavrosOffboardPosctl(object):
         self.extended_state = ExtendedState()
         self.imu_data = Imu()
         self.local_position = PoseStamped()
+        self.local_position_old = PoseStamped()
         self.state = State()
         self.rover_pos = PointStamped()
 
@@ -56,11 +60,35 @@ class MavrosOffboardPosctl(object):
                                               self.local_position_callback)
         self.state_sub = rospy.Subscriber('mavros/state', State,
                                           self.state_callback)
+        self.image_depth_sub = rospy.Subscriber("/drone/camera/depth/points", PointCloud2, self.depth_callback, queue_size=5, buff_size=52428800)
+
 
         self.ready_pub = rospy.Publisher('ready', Bool, queue_size=10)
 
         self.pos = PoseStamped()
         self.radius = 0.1
+
+        self.altitude = 0.5     # altitude used to follow rover
+        self.direction = np.array([0,0])    # direction from drone to rover
+        self.rover_timer = 0    # timer used when drone cannot see rover (initiates spin move)
+        self.x_inc = 0  # temporary x increment to p_goal
+        self.y_inc = 0  # temporary y increment to p_goal
+        self.yaw_inc = 0    # temporary yaw increment
+        self.p_goal = np.array([0,0])   # target goal position for drone (global CSYS)
+        self.yaw = 0    # yaw goal (radians)
+        self.yaw_deg = 90    # not used
+        self.roof_timer = 0     # timer used for hop manuever 
+        self.on_roof = False    # boolean used to determine if drone still over roof
+        self.roof_stage = 0     # stage of hop manuever, see hop code for more details
+        self.roof_check = 0     # timer used to double check obstacle in drone's path
+        self.local_pos_timer = 0    # timer used to take current and old drone position data (legacy)
+        self.rover = np.array([0,3])    # stored current rover position (global)
+        self.rover_old = np.array([0,0])    # stored old rover position (global)
+        self.rover_speed = np.array([0,0], dtype=float)     # calculated rover speed [xdot,ydot]
+        self.speed_check_timer = 0  # timer used to check speed of rover (legacy)
+        self.y_vel_lowpass = 0.3    # stored value of est. rover speed
+        self.drone_at_hop_start = np.array([0,0])   # location of drone at hop start (global)
+        self.drone_zrot_hop_start = 0   # z rotation of drone at hop start (global)
 
         self.pos_setpoint_pub = rospy.Publisher(
             'mavros/setpoint_position/local', PoseStamped, queue_size=1)
@@ -84,23 +112,180 @@ class MavrosOffboardPosctl(object):
         drone = np.array([
             self.local_position.pose.position.x,
             self.local_position.pose.position.y])
+        self.drone = drone
 
+        self.rover_old = self.rover
         rover = np.array([
             self.rover_pos.point.x,
             self.rover_pos.point.y])
+        self.rover = rover
+        # print(self.rover)
+
+        
+        RC = 0.5 # time constant in seconds of first order low pass filter
+        # check_time = 5
+        # if self.speed_check_timer == check_time:
+        #     self.speed_check_timer = 0
+        #     print(self.rover_speed)
+
+        dt = 1/15   # seconds
+        curr_rover_speed = (self.rover - self.rover_old) / dt
+        alpha = dt/(RC + dt)
+        self.rover_speed[0] = (1 - alpha)*self.rover_speed[0] + alpha*curr_rover_speed[0]
+        self.rover_speed[1] = (1 - alpha)*self.rover_speed[1] + alpha*curr_rover_speed[1]
+        
+        print("alpha: ", alpha)
+        print("cur_speed: ", curr_rover_speed)
+        print("lowpass_speed: ", (self.rover_speed))
 
         direction = rover - drone
         direction /= np.linalg.norm(direction)
 
         d_separation = 1.0
-        altitude = 0.5
         
-        p_goal = rover - direction*d_separation
-
-        yaw = np.arctan2(direction[1], direction[0])
+        if self.roof_stage == 0:
+            self.p_goal = rover - direction*d_separation
+            self.yaw = np.arctan2(direction[1], direction[0])
       
-        self.goto_position(x=p_goal[0], y=p_goal[1], z=altitude, yaw_deg=np.rad2deg(yaw))
+        # OLD
+        # self.goto_position(x=p_goal[0], y=p_goal[1], z=altitude, yaw_deg=(np.rad2deg(yaw) + self.yaw_inc))
 
+        # Reset rover find timer when found rover
+        self.rover_timer = 0
+        self.x_inc = 0
+        self.y_inc = 0
+        self.yaw_inc = 0
+        # self.speed_check_timer += 1
+
+    def depth_callback(self, data):
+        # points_list = []
+        # points_array = ros_numpy.point_cloud2.pointcloud2_to_array(data)
+    
+        # xyz_array = ros_numpy.point_cloud2.get_xyz_points(points_array, remove_nans=True)
+
+        xyz_array = ros_numpy.point_cloud2.pointcloud2_to_xyz_array(data, remove_nans=False)
+
+        # points_list_mod = points_list.reshape(480, 640, 3)
+
+        # center_points = points_list_mod[0:320, 213:426][0:3]
+        # print(center_points)
+        # print(center_points.shape)
+
+        center_col = xyz_array[220:260,:,:]
+
+        center_col_dist = np.linalg.norm(center_col, axis=2)
+        center_col_dist = center_col_dist[~np.isnan(center_col_dist)]
+        if center_col_dist.size == 0:
+            center_col_dist = np.nan
+
+        alt_max = 5
+        move_mult = 1
+        wait_ticks = 15
+
+        
+        # print(self.roof_stage)
+        # print(np.min(center_col_dist))
+
+        # Attempt legend:
+        # Attempt number 1 - pooly organized adaptable script
+        # Attempt number 2 - better organized adaptable script
+        # Attempt number 3 - everything hardcoded (built off attempt number 2 framework)
+
+        # ATTEMPT NUMBER 3 (Hard coded values)
+        # Roof stages
+        if self.roof_stage == 0:    # Check for obstacle
+            # print(any(center_col_dist < 0.8))
+            if np.min(center_col_dist) < 0.6:
+                # Calculate direction drone was moving
+                # drone = np.array([
+                #     self.local_position.pose.position.x,
+                #     self.local_position.pose.positionself.drone_zrot_hop_startion.x,
+                #     self.local_position_old.pose.position.y])
+                # direction = drone - drone_old
+                # self.direction = direction / np.linalg.norm(direction)
+
+                self.roof_stage += 1
+
+        elif self.roof_stage == 1:  # Make sure object not a fluke
+            # print(any(center_col_dist p.array([self.x_inc, self.y_inc])< 0.9))
+            if np.min(center_col_dist) < 0.8:
+                self.roof_check += 1
+                self.p_goal = self.drone
+                self.x_inc = 0
+                self.y_inc = 0
+            else:
+                self.roof_stage = 0
+                self.roof_check = 0
+
+            if self.roof_check > 10:
+                self.roof_stage += 1
+                self.roof_check = 0
+                drone = np.array([
+                    self.local_position.pose.position.x,
+                    self.local_position.pose.position.y])
+                self.drone_at_hop_start = drone
+
+                euler_current = euler_from_quaternion([
+                    self.local_position.pose.orientation.x,
+                    self.local_position.pose.orientation.y,
+                    self.local_position.pose.orientation.z,
+                    self.local_position.pose.orientation.w], axes='rzyx')
+                self.drone_zrot_hop_start = euler_current[0]    #radians
+
+        elif self.roof_stage == 2:  # Fly up
+            self.altitude = 1.5
+            self.roof_timer = 0
+            self.roof_stage += 1
+
+        elif self.roof_stage == 3:  # Wait a moment
+            self.roof_timer += 1
+            if self.roof_timer > wait_ticks:
+                self.roof_stage += 1
+                self.roof_timer = 0
+
+        elif self.roof_stage == 4:  # Fly over obstacle
+            hop_magnitude = 3
+            self.x_inc = np.cos(self.drone_zrot_hop_start)*hop_magnitude
+            self.y_inc = np.sin(self.drone_zrot_hop_start)*hop_magnitude
+            self.roof_stage += 1
+
+        elif self.roof_stage == 5:  # Wait a moment
+            self.roof_timer += 1
+            if self.roof_timer > wait_ticks*2:
+                self.roof_stage += 1
+                self.roof_timer = 0
+
+        elif self.roof_stage == 6:  # Fly back down
+            # if self.altitude > 0.5:
+            #     self.altitude -= 0.02*move_mult
+            # else:
+            #     self.roof_stage = 0
+            self.altitude = 0.5
+            self.roof_stage += 1
+            self.roof_timer = 0
+            est_rover = self.rover_speed*wait_ticks*3
+            # self.p_goal += self.rover + est_rover
+            self.p_goal = self.drone_at_hop_start + np.array([self.x_inc, self.y_inc])
+            # print(self.p_goal)
+            self.x_inc = 0
+            self.y_inc = 0
+
+        elif self.roof_stage == 7:  # Wait a moment
+            self.roof_timer += 1
+            if self.roof_timer > wait_ticks/2:
+                self.roof_stage = 0
+                self.roof_timer = 0
+
+        # Increment rover timer
+        self.rover_timer += 1
+
+        # Initiate search mode when cannot find rover
+        if (self.rover_timer > 10 and self.roof_stage == 0):
+            self.yaw_inc = np.mod(self.yaw_inc + 5, 360)
+        # print(self.rover_timer)
+
+        # Send pos commands to drone
+        self.goto_position(x=(self.p_goal[0] + self.x_inc), y=(self.p_goal[1] + self.y_inc), z=self.altitude, yaw_deg=(np.rad2deg(self.yaw) + self.yaw_inc))
 
     def extended_state_callback(self, data: ExtendedState):
         if self.extended_state.vtol_state != data.vtol_state:
